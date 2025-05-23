22ce4e525e9603d5d9abcf03e389cf7ee941348b
sang-sok <sangsok2222@gmail.com>
2025-05-23 18:58:25 +0700

    Your message

--- src/app/(landing)/contact/page.tsx
+++ src/app/(landing)/contact/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
                             id="email"
                             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                             placeholder="name@flowbite.com"
-                            required=""
+                            // required=""
                         />
                     </div>
                     <div>
@@ -37,7 +37,7 @@ export default function Page() {
                             id="subject"
                             className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                             placeholder="Let us know how we can help you"
-                            required=""
+                            // required=""
                         />
                     </div>
                     <div className="sm:col-span-2">
